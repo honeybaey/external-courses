8cc884ddc85f0f4895f3db5c298c86c34040c29e
honeybaey <honeybaey@gmail.com>
2020-11-09 16:12:10 +0300

    Solve task-04

---
+++ src/ex2_js-basics-part2/task-04.js
@@ -0,0 +1,10 @@
+const isDuplicate = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) return true;
+    }
+  }
+  return false;
+};
+
+module.exports = isDuplicate;
