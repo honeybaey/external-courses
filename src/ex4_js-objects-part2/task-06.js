69cc8229689afa5738f2bcb8c8df956a23a98cb7
honeybaey <honeybaey@gmail.com>
2020-11-10 14:33:33 +0300

    Solve task-06

---
+++ src/ex4_js-objects-part2/task-06.js
@@ -0,0 +1,10 @@
+const toUpEveryFirstChar = (str) => {
+  let arr = str.split(" ");
+  return arr
+    .map((elem) => {
+      return elem.charAt(0).toUpperCase() + elem.slice(1);
+    })
+    .join(" ");
+};
+
+module.exports = toUpEveryFirstChar;
