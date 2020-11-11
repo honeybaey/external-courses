28e552f7cff99e40184fd10c1b1ef0732bdde9b2
honeybaey <honeybaey@gmail.com>
2020-11-11 14:22:29 +0300

    Solve task-04

---
+++ src/ex6_js-array-methods/task-04.js
@@ -0,0 +1,11 @@
+const cloneFilter = (array, callback) => {
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array)) {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
+};
+
+module.exports = cloneFilter;
