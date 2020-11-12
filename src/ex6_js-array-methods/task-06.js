4ddd2ba5365c5f0e5f045bf7851d08f2d12ab9f5
honeybaey <honeybaey@gmail.com>
2020-11-12 11:20:59 +0300

    Solve task-06

---
+++ src/ex6_js-array-methods/task-06.js
@@ -0,0 +1,12 @@
+const cloneReduce = (array, callback, initialValue) => {
+  let previousValue = initialValue || array[0];
+  let i = initialValue ? 0 : 1;
+
+  for (; i < array.length; i++) {
+    previousValue = callback(previousValue, array[i], i, array);
+  }
+
+  return previousValue;
+};
+
+module.exports = cloneReduce;
