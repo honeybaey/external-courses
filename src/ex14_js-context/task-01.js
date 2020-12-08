7b5cb393dffbaaa961be3a8fb39b89da95f3e8ae
honeybaey <honeybaey@gmail.com>
2020-12-08 11:33:44 +0300

    Solve task-01

---
+++ src/ex14_js-context/task-01.js
@@ -0,0 +1,68 @@
+function Calc() {
+  this.state = 0;
+
+  this.add = function (x) {
+    if (arguments.length === 0) {
+      this.state;
+    } else {
+      this.state += x;
+    }
+    return this;
+  };
+
+  this.subtract = function (x) {
+    if (arguments.length === 0) {
+      this.state;
+    } else {
+      this.state -= x;
+    }
+    return this;
+  };
+
+  this.divide = function (x) {
+    if (arguments.length === 0) {
+      this.state;
+    } else {
+      this.state /= x;
+    }
+    return this;
+  };
+
+  this.multiply = function (x) {
+    if (arguments.length === 0) {
+      this.state;
+    } else {
+      this.state *= x;
+    }
+    return this;
+  };
+
+  this.getResult = function () {
+    return this.state;
+  };
+
+  this.reset = function () {
+    this.state = 0;
+    return this;
+  };
+
+  this.setState = function (x) {
+    this.state = x;
+    return this;
+  };
+
+  this.fetchData = function (callback) {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => callback(json));
+    return this.setState(500);
+  };
+}
+
+const showResponse = (json) => {
+  console.log(json);
+};
+
+let Calculator = new Calc();
+
+Calculator.fetchData(showResponse);
