49c0e9edfd482edf8205119bae332a7a85eb4c11
honeybaey <honeybaey@gmail.com>
2020-12-14 15:42:40 +0300

    Solved task-09

---
+++ src/ex4_js-objects-part2/task-09.js
@@ -0,0 +1,12 @@
+const getExtendedString = (parentStr, childrenStr, index) => {
+  let str = parentStr.split(" ");
+  let newStr;
+
+  newStr = `${str.slice(0, index + 1)} ${childrenStr} ${str.slice(
+    index + 1,
+    str.length
+  )}`;
+  return newStr.split(",").join(" ");
+};
+
+module.exports = getExtendedString;
