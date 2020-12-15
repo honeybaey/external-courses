797af119c650d6e093abc15a7c9ac601e5a97a3e
honeybaey <honeybaey@gmail.com>
2020-12-15 15:29:49 +0300

    Solved task

---
+++ src/ex5_js-functions/task.js
@@ -0,0 +1,30 @@
+const calc = {
+  state: 0,
+
+  add: function (x = 0) {
+    this.state += x;
+    return calc.add;
+  },
+  subtract: function (x = 0) {
+    calc.state -= x;
+    return calc.subtract;
+  },
+  divide: function (x = 1) {
+    calc.state /= x;
+    return calc.divide;
+  },
+  multiply: function (x = 1) {
+    calc.state *= x;
+    return calc.multiply;
+  },
+  getResult: function () {
+    return calc.state;
+  },
+  reset: function () {
+    return (calc.state = 0);
+  },
+};
+
+const Calculator = calc;
+
+module.exports = Calculator;
