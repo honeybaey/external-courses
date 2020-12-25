4d97f3449f2fdca5e52c9cbdd411dd6e3e77779d
honeybaey <honeybaey@gmail.com>
2020-12-25 12:27:28 +0300

    Added checking

--- src/ex4_js-objects-part2/task-06.js
+++ src/ex4_js-objects-part2/task-06.js
@@ -1,10 +1,14 @@
 const capitalizeEachFirstChar = (str) => {
-  let arr = str.split(" ");
-  return arr
-    .map((elem) => {
-      return elem.charAt(0).toUpperCase() + elem.slice(1);
-    })
-    .join(" ");
+  if (/[^a-zа-яё\s]+/i.test(str)) {
+    return "Введите корректную строку";
+  } else {
+    let arr = str.split(" ");
+    return arr
+      .map((elem) => {
+        return elem.charAt(0).toUpperCase() + elem.slice(1);
+      })
+      .join(" ");
+  }
 };
 
 module.exports = capitalizeEachFirstChar;
